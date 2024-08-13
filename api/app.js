95ea785d45d13251593253e3078de986c5794232
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-08-13 16:27:00 +0530

    Initialized Express server with Prisma for database interaction, Nodemon for auto-restarts, CORS for handling cross-origin requests, and Axios for HTTP client support. Added a basic controller to test client-side interaction with the database and set up initial routes for API endpoints.

---
+++ api/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import postRoute from "./routes/post.route.js";
+import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL, // Ensure this matches the front-end URL
+    credentials: true, // Allow credentials (cookies, etc.)
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(8800, () => {
+  console.log("Server is running");
+});
